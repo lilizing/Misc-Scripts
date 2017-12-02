f85c781f04fa02e4363afc934e5001b42df91b1c
Leo <lilizing@qq.com>
2017-12-02 21:38:48 +0800

    feat: 优化anki脚本
    Signed-off-by: Leo <lilizing@qq.com>

--- ImportAnki.py
+++ ImportAnki.py
@@ -9,45 +9,68 @@ import csv
 #from sys import argv
 import glob
 import time
+
+# sys.setdefaultencoding('utf-8')
+
 output = "Tobeimport.csv"
 media_dir = "/Users/leo/Library/Application Support/Anki2/用户1/collection.media"
 
+reload(sys)  
+sys.setdefaultencoding("utf8")  
+  
+def translate(str):  
+    line = str.strip().decode('utf-8', 'ignore')  # 处理前进行相关的处理，包括转换成Unicode等  
+    p2 = re.compile(ur'[^\u4e00-\u9fa5\uff00-\uffef]')  # 中文的编码范围是：\u4e00到\u9fa5，中文标点符号范围是：\uff00-\uffef
+    zh = "".join(p2.split(line)).strip() 
+    return zh
+    
 def Main():
     curpath = os.getcwd()
     lrcfiles = curpath + "//*.lrc"
     flist = glob.glob(lrcfiles)
     writer = csv.writer(file(output, 'wb+'))
     pattern1 = re.compile(r'00]([\s\S]*) \t')
-    pattern2 = re.compile(r'\t([\s\S]*)')
+
     i = 0
     for f in flist:
        i = i + 1
-       fh = file(f,'r')
+       
+       newname_prefix = str(time.time()) + str(i)
+
+       print "***********step1: 字幕处理 - 转码、重命名、提取中英文***********"
+       newF = newname_prefix + '.lrc'
+       os.system('iconv -f GB2312 -t UTF-8 ' + f + ' > ' + newF)
+       
+       fh = file(newF,'r')
        content = fh.read()
        english = pattern1.findall(content)
-       chinese = pattern2.findall(content)
+       chinese = translate(content)
 
        #print content
-
        #print english
-       #print chinese 
+       #print chinese
+
+       print "***********step2: 音频处理 - 重命名音频文件***********"
        mp3file = f[:-3] + "mp3"
-       newname = str(time.time()) + str(i) + ".mp3"
+       newname = newname_prefix + ".mp3"
+
        #print mp3file
        #print newname
+       
+       os.remove(os.path.join(curpath, f))
        os.rename(mp3file, newname)
        question = "[sound:" + newname +"]"
 
        if not english:
-	   answer =content[10:]
+	        answer = content[10:]
        else:
-	   answer = english[0] + "   " + question
-       writer.writerow([question, answer])
+	        answer = question + "   " + english[0]
+       writer.writerow([question, question + "  " + chinese, answer])
        fh.close()
     del writer
-    print "***********复制音频文件到媒体库***********"
-    copyFiles(os.getcwd(), media_dir)
-      
+    print "***********step3: 复制音频文件到媒体库***********"
+    # copyFiles(os.getcwd(), media_dir)
+    
 def copyFiles(sourceDir,  targetDir):
     for file in os.listdir(sourceDir): 
         if os.path.splitext(file)[1] != ".mp3":
