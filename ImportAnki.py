4f8ef4954ecc0e79c266bddf2571078d27ced005
Leo <lilizing@qq.com>
2017-12-03 21:12:12 +0800

    fix: optimize script for anki
    Signed-off-by: Leo <lilizing@qq.com>

--- ImportAnki.py
+++ ImportAnki.py
@@ -17,61 +17,7 @@ media_dir = "/Users/leo/Library/Application Support/Anki2/用户1/collection.med
 
 reload(sys)  
 sys.setdefaultencoding("utf8")  
-  
-def translate(str):  
-    line = str.strip().decode('utf-8', 'ignore')  # 处理前进行相关的处理，包括转换成Unicode等  
-    p2 = re.compile(ur'[^\u4e00-\u9fa5\uff00-\uffef]')  # 中文的编码范围是：\u4e00到\u9fa5，中文标点符号范围是：\uff00-\uffef
-    zh = "".join(p2.split(line)).strip() 
-    return zh
-    
-def Main():
-    curpath = os.getcwd()
-    lrcfiles = curpath + "//*.lrc"
-    flist = glob.glob(lrcfiles)
-    writer = csv.writer(file(output, 'wb+'))
-    pattern1 = re.compile(r'00]([\s\S]*) \t')
 
-    i = 0
-    for f in flist:
-       i = i + 1
-       
-       newname_prefix = str(time.time()) + str(i)
-
-       print "***********step1: 字幕处理 - 转码、重命名、提取中英文***********"
-       newF = newname_prefix + '.lrc'
-       os.system('iconv -f GB2312 -t UTF-8 ' + f + ' > ' + newF)
-       
-       fh = file(newF,'r')
-       content = fh.read()
-       english = pattern1.findall(content)
-       chinese = translate(content)
-
-       #print content
-       #print english
-       #print chinese
-
-       print "***********step2: 音频处理 - 重命名音频文件***********"
-       mp3file = f[:-3] + "mp3"
-       newname = newname_prefix + ".mp3"
-
-       #print mp3file
-       #print newname
-       
-       os.remove(os.path.join(curpath, f))
-       os.rename(mp3file, newname)
-       question = "[sound:" + newname +"]"
-
-       if not english:
-	        answer = content[10:]
-       else:
-	        answer = english[0]
-       writer.writerow([question, chinese, answer])
-       
-       fh.close()
-    del writer
-    print "***********step3: 复制音频文件到媒体库***********"
-    copyFiles(os.getcwd(), media_dir)
-    
 def copyFiles(sourceDir,  targetDir):
     for file in os.listdir(sourceDir): 
         if os.path.splitext(file)[1] == ".csv":
@@ -86,6 +32,61 @@ def copyFiles(sourceDir,  targetDir):
         if os.path.isdir(sourceFile): 
             First_Directory = False 
             copyFiles(sourceFile, targetFile)
+  
+def GetChinese(str):  
+    line = str.strip().decode('utf-8', 'ignore')  # 处理前进行相关的处理，包括转换成Unicode等  
+    p2 = re.compile(ur'[^\u4e00-\u9fa5\uff00-\uffef]')  # 中文的编码范围是：\u4e00到\u9fa5，中文标点符号范围是：\uff00-\uffef
+    zh = "".join(p2.split(line)).strip() 
+    return zh
+
+def GetEnglish(content):
+    value = content.strip().decode('utf-8', 'ignore')
+    m = re.match(ur'([^\u4e00-\u9fa5])*', value)
+    return m.group().strip()[10:]
+    
+def Main():
+    curpath = os.getcwd()
+    lrcfiles = curpath + "//*.lrc"
+    flist = glob.glob(lrcfiles)
+    writer = csv.writer(file(output, 'wb+'))
+    pattern1 = re.compile(r'00]([\s\S]*) \r')
+
+    i = 0
+    for f in flist:
+       i = i + 1
+       
+       newname_prefix = str(time.time()) + str(i)
+
+       print "***********step1: 字幕处理 - 转码、重命名、提取中英文***********"
+       newF = newname_prefix + '.lrc'
+    #    os.system('iconv -f GB2312 -t UTF-8 ' + f + ' > ' + newF)
+    #    os.remove(os.path.join(curpath, f))
+
+       os.rename(f, newF)
+
+       fh = file(newF,'r')
+       content = fh.read()
+
+       english = GetEnglish(content)
+       chinese = GetChinese(content)
+
+       print "***********step2: 音频处理 - 重命名音频文件***********"
+       mp3file = f[:-3] + "mp3"
+       newname = newname_prefix + ".mp3"
+       
+       os.rename(mp3file, newname)
+       question = "[sound:" + newname +"]"
+
+       writer.writerow([question, chinese, english])
+       
+       fh.close()
+
+    del writer
+    
+    print "***********step3: 复制音频文件到媒体库***********"
+    copyFiles(os.getcwd(), media_dir)
+    
+
 
 if __name__ == "__main__":
     Main()
