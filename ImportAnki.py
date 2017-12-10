ed037877b0700fb07f5b3be80153aa92b1fac4d0
Leo <lilizing@qq.com>
2017-12-10 15:27:11 +0800

    fix
    Signed-off-by: Leo <lilizing@qq.com>

--- ImportAnki.py
+++ ImportAnki.py
@@ -6,17 +6,14 @@ import re
 import os
 import sys
 import csv
-#from sys import argv
 import glob
 import time
 
-# sys.setdefaultencoding('utf-8')
+reload(sys)
+sys.setdefaultencoding("utf8") 
 
-output = "Tobeimport.csv"
+output = "import.csv"
 media_dir = "/Users/leo/Library/Application Support/Anki2/用户1/collection.media"
-
-reload(sys)  
-sys.setdefaultencoding("utf8")  
 
 def copyFiles(sourceDir,  targetDir):
     for file in os.listdir(sourceDir): 
@@ -42,7 +39,7 @@ def GetChinese(str):
 def GetEnglish(content):
     value = content.strip().decode('utf-8', 'ignore')
     m = re.match(ur'([^\u4e00-\u9fa5])*', value)
-    return m.group().strip()[10:]
+    return m.group().strip()[11:]
     
 def Main():
     curpath = os.getcwd()
@@ -51,40 +48,50 @@ def Main():
     writer = csv.writer(file(output, 'wb+'))
     pattern1 = re.compile(r'00]([\s\S]*) \r')
 
+    # originCode = 'UTF-8'
+    # if len(sys.argv) > 1:
+    #     originCode = sys.argv[1]
+    # print "***********原编码：" + originCode + "***********"
+
     i = 0
     for f in flist:
        i = i + 1
-       
+
        newname_prefix = str(time.time()) + str(i)
 
-       print "***********step1: 字幕处理 - 转码、重命名、提取中英文***********"
+       print "***********step1: 转码、提取中英文***********"
        newF = newname_prefix + '.lrc'
-       os.system('iconv -f GB2312 -t UTF-8 ' + f + ' > ' + newF)
-       os.remove(os.path.join(curpath, f))
+        #    os.system('iconv -f ' + originCode + ' -t UTF-8 "' + f + '" > "' + newF + '"')
+        #    os.remove(os.path.join(curpath, f))
        
-       #test code
-       #    os.rename(f, newF)
+       os.system('enca -L zh_CN -x UTF-8 "' + f + '"')
 
-       fh = file(newF,'r')
+       fh = file(f,'rw')
        content = fh.read()
 
        english = GetEnglish(content)
        chinese = GetChinese(content)
 
-       print "***********step2: 音频处理 - 重命名音频文件***********"
+        #    print content
+        #    print english
+        #    print chinese
+       
+       print "***********step2: 重命名文件名***********"
+       os.rename(f, newF)
+
        mp3file = f[:-3] + "mp3"
        newname = newname_prefix + ".mp3"
        
        os.rename(mp3file, newname)
-       question = "[sound:" + newname +"]"
+       audio = "[sound:" + newname +"]"
 
-       writer.writerow([question, chinese, english])
+       writer.writerow([audio, english, chinese])
        
        fh.close()
 
     del writer
 
-    print "***********step3: 复制音频文件到媒体库***********"
+    print "***********step3: 复制文件到媒体库***********"
     copyFiles(os.getcwd(), media_dir)
     
 
